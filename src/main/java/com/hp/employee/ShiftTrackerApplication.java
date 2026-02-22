edce36d31bec2e8f84eb9a4a755c534c44ef92e4
HPK017 <parelharsh0250@gmail.com>
2026-02-22 09:35:47 +0530

    Initail SetUp for Employee Shift Tracker Application

---
+++ src/main/java/com/hp/employee/ShiftTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hp.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftTrackerApplication.class, args);
+	}
+
+}
