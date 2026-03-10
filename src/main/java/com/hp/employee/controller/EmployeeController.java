c16664917fbb7beae9607dadf362d9162991fabe
HPK017 <parelharsh0250@gmail.com>
2026-03-10 19:23:24 +0530

    employee apis are created

---
+++ src/main/java/com/hp/employee/controller/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.hp.employee.controller;
+
+import com.hp.employee.dto.EmployeeRequestDto;
+import com.hp.employee.dto.EmployeeResponseDto;
+import com.hp.employee.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping("create")
+    public EmployeeResponseDto createEmployee(@RequestBody EmployeeRequestDto employeeRequestDto) {
+        return employeeService.createEmployee(employeeRequestDto);
+    }
+
+    @GetMapping("/{id}")
+    public EmployeeResponseDto getEmployeeId(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @GetMapping("/getAllEmployees")
+    public List<EmployeeResponseDto> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PutMapping("/{id}")
+    public EmployeeResponseDto updateEmployee(@PathVariable Long id,
+                                              @RequestBody EmployeeRequestDto employeeRequestDto) {
+        return employeeService.updateEmployee(id, employeeRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+}
