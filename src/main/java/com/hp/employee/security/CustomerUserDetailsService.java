bb2796530962956aed2df0ebfa606e3a293a94d3
HPK017 <parelharsh0250@gmail.com>
2026-03-11 22:37:31 +0530

    load the users using userdetails

---
+++ src/main/java/com/hp/employee/security/CustomerUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.hp.employee.security;
+
+import com.hp.employee.entity.Employee;
+import com.hp.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Employee employee = employeeRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return new User(
+                employee.getEmail(),
+                employee.getPassword(),
+                List.of(new SimpleGrantedAuthority(employee.getRole().name()))
+        );
+    }
+}
