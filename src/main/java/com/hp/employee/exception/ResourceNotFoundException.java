fdfcabf577b79b88f50d5be90a1e42cbb67ca030
HPK017 <parelharsh0250@gmail.com>
2026-03-16 21:18:16 +0530

    custom handler for the resources

---
+++ src/main/java/com/hp/employee/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.hp.employee.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
