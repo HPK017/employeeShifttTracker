ba31d8735a44b903ab5798c3b4707bf4560978ba
HPK017 <parelharsh0250@gmail.com>
2026-03-20 20:29:02 +0530

    time sheet entity updated

--- src/main/java/com/hp/employee/entity/TimeSheet.java
+++ src/main/java/com/hp/employee/entity/TimeSheet.java
@@ -37,8 +37,6 @@ public class TimeSheet {
 
     private LocalDateTime fromDate;
 
-    private LocalDateTime toDate;
+    private LocalDateTime endDate;
 
-    @Column(nullable = false)
-    private Double actualHours;
 }
