a03f68a4cb5d6c1b0edd8f6a0b39495fd6784259
HPK017 <parelharsh0250@gmail.com>
2026-03-24 20:33:13 +0530

    query to get time between for generate report

--- src/main/java/com/hp/employee/repository/ShiftRepository.java
+++ src/main/java/com/hp/employee/repository/ShiftRepository.java
@@ -4,6 +4,7 @@ import com.hp.employee.entity.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.w3c.dom.stylesheets.LinkStyle;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,4 +14,6 @@ public interface ShiftRepository extends JpaRepository<Shift, Long> {
     Optional<Shift> findFirstByEmployeeIdAndEndTimeIsNullOrderByStartTimeDesc(Long employeeId);
 
     Optional<Shift> findByEmployeeId(Long employeeId);
+
+    List<Shift> findByStartTimeBetween(LocalDateTime start, LocalDateTime end);
 }
