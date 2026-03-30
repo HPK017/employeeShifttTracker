279c98f6d5b36848bd9e4623eb0a744f07da8984
HPK017 <parelharsh0250@gmail.com>
2026-03-30 19:23:40 +0530

    refresh token stored in DB

---
+++ src/main/java/com/hp/employee/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.hp.employee.repository;
+
+import com.hp.employee.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByToken(String token);
+
+    void deleteByEmail(String email);
+}
